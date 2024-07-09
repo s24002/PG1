717599f5a40390c666d1d7431b06e8e734f93e74
ike <s24002@std.it-college.ac.jp>
2024-07-09 06:37:01 +0000

    四日目

---
+++ 2-11.py
@@ -0,0 +1,15 @@
+import random
+
+print(random. randint(0,9))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+print(random.choice(alphabet))
+print(random.sample(alphabet,17))
+
+def fubc(v):
+    return v + 3
+import my_module
+print(my_module.func(5))
+
+from random import randint, choice
+print(randint(0,10))
+print(choice(['a','b','c']))
---
+++ 2-13-1.py
@@ -0,0 +1,5 @@
+import random as rd
+from random import randint as rad
+
+print(rd.randint(0,10))
+print(rad(0,10))
