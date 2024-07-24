d6248a718ba8c7f93cb88960f806a1228c00841e
ike <s24002@std.it-college.ac.jp>
2024-07-24 07:57:41 +0000

    7/24(wed)

---
+++ 5-9.py
@@ -0,0 +1,13 @@
+mountain_in_japan = {'fuji': 3776, 'kitadake': 3197, 'okuhodakadake': 3190}
+mountain_in_japan_sorted = sorted(mountain_in_japan.items(), key=lambda x: x[0])
+for key, val in mountain_in_japan_sorted:
+    print(key,val)
+
+mountain_in_japan = {'fuji': 3776, 'kitadake': 3197, 'okuhodakadake': 3190}
+mountain_in_japan_sorted = sorted(mountain_in_japan.items(), key=lambda x: x[1])
+for key, val in mountain_in_japan_sorted:
+    print(key,val)
+
+for key, val in reversed(mountain_in_japan_sorted):
+    print(key,val)
+
